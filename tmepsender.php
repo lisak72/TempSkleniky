6df96d7d43e7a1cd6f02d7223e25c2e5d888e4ab
Jiri Liska <lisak72@seznam.cz>
2020-10-12 19:23:14 +0200

    tmpsender php file sending data to tmep.cz map added

---
+++ tmepsender.php
@@ -0,0 +1,32 @@
+<?php
+//$templ = file_get_contents('http://192.168.200.123:8247/');
+//echo $templ;
+$handle = fopen('http://192.168.200.123:8247','r');
+stream_set_timeout($handle, 2);
+if($handle==FALSE) {
+ $handle=fopen('./fail1.html','r');
+    }
+$contents = stream_get_contents($handle);
+fclose($handle);
+
+$str=$contents;
+$from="Teplota venkovni sever:";
+$to="&deg;C";
+$contents=getStringBetween($str,$from,$to);
+
+function getStringBetween($str,$from,$to)
+{
+    $sub = substr($str, strpos($str,$from)+strlen($from),strlen($str));
+    return substr($sub,0,strpos($sub,$to));
+}
+$contents=trim($contents);
+$contents=floatval($contents);
+//echo $contents;  //in contents is now variable number of outside temperature - number in C grad
+
+$handle2 = fopen('http://butbn.tmep.cz/?skleniky_venku='.$contents,'r');
+$response=stream_get_contents($handle2);
+fclose($handle2);
+
+echo $response;
+exit
+?>
